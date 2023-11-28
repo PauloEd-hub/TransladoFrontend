889da69c5be398afe35affe24da1162e4aea6111
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-11-27 23:21:40 -0300

    Troca framework css para UIKIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
 import { HeaderComponent } from './components/header/header.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import { FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TransladoComponent } from './pages/translado/translado.component';
 import { HomeComponent } from './pages/home/home.component';
-import {MatGridListModule} from '@angular/material/grid-list';
 import { CarrosComponent } from './pages/carros/carros.component';
 import { FormVeiculosComponent } from './components/form-veiculos/form-veiculos.component';
-import {MatDividerModule} from '@angular/material/divider';
 import { FormTransladoComponent } from './components/form-translado/form-translado.component';
 import { FormDestinoComponent } from './components/form-destino/form-destino.component';
-import {MatSelectModule} from '@angular/material/select';
+
 
 
 
@@ -46,17 +38,9 @@ import {MatSelectModule} from '@angular/material/select';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatGridListModule,
-    MatDividerModule,
-    MatSelectModule
+    
     
   ],
   providers: [],
--- src/app/components/form-veiculos/form-veiculos.component.ts
+++ src/app/components/form-veiculos/form-veiculos.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class FormVeiculosComponent {
 
+
+  constructor() {
+
+  }
+
+
+
+  registerCarro() {
+    
+  }
+
 }
